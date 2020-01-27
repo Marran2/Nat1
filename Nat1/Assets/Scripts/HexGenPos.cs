06cdcb0c099861118714ba30864596f27cfa5ff8
Martin Hultgren <marhu242@student.liu.se>
2020-01-27 19:46:20 +0100

    Added some materials and addons

---
+++ Nat1/Assets/Scripts/HexGenPos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGenPos : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
